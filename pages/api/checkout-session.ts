3a20dc8f425e5c2cd346e1b9ac35e8b37d7f26b1
LyatifAhmed <latifmutlu@gmail.com>
2025-08-01 20:24:14 +0100

    Fix Stripe API version

--- pages/api/checkout-session.ts
+++ pages/api/checkout-session.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-03-31.basil", // ✅ Stable recommended
+  apiVersion: "2025-07-30.basil", // ✅ Stable recommended
 });
 
 type CheckoutRequestBody = {
